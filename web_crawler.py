d17ad016edc9af0ac2df3702be60273cc1c49a13
[Gemma-Rate] <thebrickmaster@theratefamily.co.uk>
2018-04-28 17:41:25 +0100

    Now can get data from wiki data.

--- web_crawler.py
+++ web_crawler.py
@@ -1,13 +1,70 @@
-import urllib
-from bs4 import BeautifulSoup
-import nltk as np
+import pywikibot
 
-qpage = 'http://www.nndb.com/people/589/000172073/'
-# Page we want to visit.
-with urllib.request.urlopen(qpage) as response:
-    html = response.read()
 
-print(html)
 
-soup = BeautifulSoup(html, 'html.parser')
-print(soup.get_text())
+class People(object):
+
+    def __init__(self):
+        self.names = None
+        self.birth_lists = None
+        self.job_lists = None
+
+        self.site = pywikibot.Site("en", "wikipedia")
+        # Make site object to get the data from.
+
+    def find_people(self):
+        pass
+
+    def get_person_info(self, name):
+        """Get information for a specific person."""
+
+        page = pywikibot.Page(self.site, name)
+        item = pywikibot.ItemPage.fromPage(page)
+        item_dict = item.get()
+        clm_dict = item_dict["claims"]
+        # Get things from all the boxes on the page.
+
+        birth_date = clm_dict["P569"]
+        # Get things from the box on date of birth.
+        for binfo in birth_date:
+            b = binfo.getTarget()
+            # Get info from target.
+            year_birth = b.year
+            month_birth = b.month
+            day_birth = b.day
+            hour_birth = b.hour
+            minute_birth = b.minute
+            second_birth = b.second
+            birth_date = [second_birth, minute_birth, hour_birth, day_birth,
+                          month_birth, year_birth]
+            # Get birth date information (up from second to year).
+
+        birth_location = clm_dict["P19"]
+        # Get things from the box on location of birth.
+        for binfo in birth_location:
+            b = binfo.getTarget()
+            bp_id = b.id
+            # Get ID of birth location (for use in coordinate location).
+            self.get_location_info(bp_id)
+
+    def get_location_info(self, identify):
+        """Get location coordinates for astrology"""
+        repo = self.site.data_repository()
+        item = pywikibot.ItemPage(repo, identify)
+
+        item_dict = item.get()
+        clm_dict = item_dict["claims"]
+        # Get things from all the boxes on the page.
+
+        coords = clm_dict["P625"]
+        # Get things from the box on date of birth.
+        for c in coords:
+            c_data = c.getTarget()
+            lat = c_data.lat
+            long = c_data.long
+            alt = c_data.alt
+            print(c_data.__dict__.keys())
+
+c = People()
+
+c.get_person_info('Albert Einstein')
